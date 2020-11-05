1a6d9fd0cf41f25f17f0aa1e41a23d35edcf9074
Sampo Pyysalo <sampo.pyysalo@utu.fi>
2020-11-05 17:07:00 +0200

    Initial import

---
+++ scripts/slurm-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
+#SBATCH -p gputest
+#SBATCH -t 00:10:00
+#SBATCH --gres=gpu:v100:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --account=project_2001426
+#SBATCH -o logs/%j.out
+#SBATCH -e logs/%j.err
+
+rm -f logs/latest.out logs/latest.err
+ln -s "$SLURM_JOBID.out" "logs/latest.out"
+ln -s "$SLURM_JOBID.err" "logs/latest.err"
+
+module purge
+module load tensorflow/2.2-hvd
+source transformers3.4/bin/activate
+
+
+echo "START $SLURM_JOBID: $(date)"
+
+srun python train.py "$@"
+
+gpuseff $SLURM_JOBID
+
+echo "END $SLURM_JOBID: $(date)"
