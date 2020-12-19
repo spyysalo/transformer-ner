56bc47e227cb4abf767d4588dd3d2f033b8a1b36
Sampo Pyysalo <sampo.pyysalo@utu.fi>
2020-12-19 10:21:37 +0200

    Model save/load

--- model.py
+++ model.py
@@ -1,17 +1,46 @@
+import os
 import math
+import json
+
+from transformers import AutoConfig, AutoTokenizer, TFAutoModel
+from transformers.optimization_tf import AdamWeightDecay
 
 from util import timed
 
 
 @timed
 def load_pretrained(model_name, cache_dir=None):
-    from transformers import AutoConfig, AutoTokenizer, TFAutoModel
+    from transformers import TFBertModel
 
-    config = AutoConfig.from_pretrained(model_name, cache_dir=cache_dir)
-    tokenizer = AutoTokenizer.from_pretrained(model_name, config=config,
-                                              cache_dir=cache_dir)
-    model = TFAutoModel.from_pretrained(model_name, config=config,
-                                        cache_dir=cache_dir)
+    config = AutoConfig.from_pretrained(
+        model_name,
+        cache_dir=cache_dir
+    )
+    # `use_fast=False` here because the "fast" tokenizer encode()
+    # doesn't accept list of strings as an argument. TODO: rework the
+    # use of encode() to be compatible with the fast tokenizers.
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name,
+        config=config,
+        cache_dir=cache_dir,
+        use_fast=False
+    )
+    model = TFAutoModel.from_pretrained(
+        model_name,
+        config=config,
+        cache_dir=cache_dir
+    )
+
+    # Transformers doesn't support saving models wrapped in keras
+    # (https://github.com/huggingface/transformers/issues/2733) at the
+    # time of this writing. As a workaround, use the main layer
+    # instead of the model. As the main layer has different names for
+    # different models (TFBertModel.bert, TFRobertaModel.roberta,
+    # etc.), this has to check which model we're dealing with.
+    if isinstance(model, TFBertModel):
+        model = model.bert
+    else:
+        raise NotImplementedError(f'{model.__class__.__name__}')
 
     return model, tokenizer, config
 
@@ -66,3 +95,68 @@ def build_ner_model(pretrained_model, num_labels, seq_len):
         outputs=[ner_output]
     )
     return ner_model
+
+
+def _ner_model_path(ner_model_dir):
+    return os.path.join(ner_model_dir, 'model.hdf5')
+
+
+def _ner_labels_path(ner_model_dir):
+    return os.path.join(ner_model_dir, 'labels.txt')
+
+
+#def _ner_model_config_path(ner_model_dir):
+#    return os.path.join(ner_model_dir, 'model_config.json')
+
+
+def _ner_viterbi_path(ner_model_dir):
+    return os.path.join(ner_model_dir, 'probs.json')
+
+
+def save_ner_model(directory, ner_model, tokenizer, labels, config):
+    os.makedirs(directory, exist_ok=True)
+    config.save_pretrained(directory)
+    tokenizer.save_pretrained(directory)
+    # with open(_ner_model_config_path(directory), 'w') as out:
+    #     json.dump(config.to_dict(), out, indent=4)
+    ner_model.save(_ner_model_path(directory))
+    with open(_ner_labels_path(directory), 'w') as out:
+        for label in labels:
+            print(label, file=out)
+
+
+def _get_custom_objects():
+    """Get custom objects for loading saved model."""
+    return  {
+        'AdamWeightDecay': AdamWeightDecay,
+    }
+
+
+def load_labels(path):
+    labels = []
+    with open(path) as f:
+        for line in f:
+            line = line.strip()
+            if line.isspace() or not line:
+                continue
+            if line in labels:
+                raise ValueError(f'duplicate value {line} in {path}')
+            labels.append(line)
+    return labels
+
+
+def load_ner_model(ner_model_dir):
+    from tensorflow.keras.models import load_model
+
+    config = AutoConfig.from_pretrained(ner_model_dir)
+    tokenizer = AutoTokenizer.from_pretrained(
+        ner_model_dir,
+        config=config,
+        use_fast=False
+    )
+    ner_model = load_model(
+        _ner_model_path(ner_model_dir),
+        custom_objects=_get_custom_objects()
+    )
+    labels = load_labels(_ner_labels_path(ner_model_dir))
+    return ner_model, tokenizer, labels, config
